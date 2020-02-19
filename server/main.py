955946aac093e2793c4999480f0a8a9e2272066e
Garrett Cook <garrettalexcook@gmail.com>
2020-02-19 15:57:47 -0500

    Add /items

--- server/main.py
+++ server/main.py
@@ -1,11 +1,17 @@
-from flask import Flagk
+from flask import Flask, jsonify
+import json
+
 app = Flask(__name__)
+
+data = json.load(open('data.json', 'r'))
 
 
 @app.route("/")
 def handle_root():
   return "test"
 
+@app.route("/items"):
+  return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
